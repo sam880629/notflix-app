c21b4a1555c84488e8d52f6255374019495a881c
sam880629 <sam880629@gmail.com>
2024-07-12 16:02:07 +0800

    新增header和一些設置

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class=" text-gray-50 flex h-16">
+    <a href="/" class="inline-block w-20 mr-6">
+      <img class="h-full" alt="Vue logo" src="../assets/logo.svg" />
+    </a>
+
+    <div class="flex gap-5 font-bold text-sm justify-center items-center  text-[#e5e5e5] ">
+      <p>首頁</p>
+      <p>節目</p>
+      <p>電影</p>
+      <p>最新熱門電影</p>
+      <p>我的口袋名單</p>
+      <p>按照語言瀏覽</p>
+    </div>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style>
+
+</style>
